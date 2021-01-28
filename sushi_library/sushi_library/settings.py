b17301851b38de8df02bc0ecff609b66145dc488
chez-shanpu <cheztomo513@gmail.com>
2021-01-28 17:13:01 +0900

    Export secret-key to env var

--- sushi_library/sushi_library/settings.py
+++ sushi_library/sushi_library/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tvom$xy3*nj!c07s6nxd+)hjatd!&32l&3^mqev5++@nd&l+s9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
