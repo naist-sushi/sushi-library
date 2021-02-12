0f01fd5999cb6378721d277a34090141c56134df
chez-shanpu <cheztomo513@gmail.com>
2021-02-12 15:48:24 +0900

    Add event & event-type models

--- sushi_library/library/admin.py
+++ sushi_library/library/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Book, BookRequest, BookRequestImportance
+from .models import Book, BookRequest, BookRequestImportance, Event, EventMessage
 
 admin.site.register(Book)
 admin.site.register(BookRequest)
 admin.site.register(BookRequestImportance)
+admin.site.register(Event)
+admin.site.register(EventMessage)
