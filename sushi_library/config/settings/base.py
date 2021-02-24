fbe58ddfe786b14f62c7779505a276423d00988d
eedy8888 <eedy7798@gmail.com>
2021-02-24 22:13:53 +0900

    <improve ui>

--- sushi_library/config/settings/base.py
+++ sushi_library/config/settings/base.py
@@ -140,11 +140,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')),
 
-STATIC_URL = '/static/'
-
-LOGIN_REDIRECT_URL = '/'
-ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
+STATIC_URL = ('/static/')
+LOGIN_REDIRECT_URL = ('/')
+ACCOUNT_LOGOUT_REDIRECT_URL = ('/accounts/login/')
 
 # MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
